053821c43d2b01f0e8d883324e974047e6bfe4ce
Raunak-Shalya <raunakshalya20@ece.iiitp.ac.in>
2024-02-12 12:55:24 +0530

    Used Axios to catch the data sent from backend

--- backend/Hospital_Management_System/src/main/java/com/hospital/management/system/controller/Controller.java
+++ backend/Hospital_Management_System/src/main/java/com/hospital/management/system/controller/Controller.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins="http://localhost:5173" ,maxAge = 3600)
 @RestController
 public class Controller {
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "../styles/Home.scss";
 import NavBar from "../components/Navbar";
+import axios from "axios";
+const baseURL = "http://localhost:8080";
+
 const Home = () => {
+  const [myData, setMyData] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get("http://localhost:8080/page/0");
+      setMyData(response.data);
+    } catch (error) {
+      console.log("Axios API Failed");
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="box">
       <NavBar />
-
       <div className="homebox">
-        <div className="homebackground"> </div>
         <button className="button-71">+ Add</button>
       </div>
     </div>
